9b20f29cbe751c0f6007afaf7a89c65dd632fcda
PhamKhac <phamkhac2003@gmail.com>
2025-03-13 15:21:52 +0700

    dựng khung layout mặc định, xóa các file không cần

--- src/components/Layout/DefaultLayout/Sidebar/index.jsx
+++ src/components/Layout/DefaultLayout/Sidebar/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import styles from './Sidebar.module.scss'
+import classNames from 'classnames/bind';
+
+const cx = classNames.bind(styles);
 
 const index = () => {
     return (
         <div>
+            <aside className={cx('wrapper')}>
             <h2>Sidebar</h2>
+            </aside>
         </div>
     );
 };
--- src/components/Layout/components/Header/index.jsx
+++ src/components/Layout/components/Header/index.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss'
+
+const cx = classNames.bind(styles)
 
 const index = () => {
     return (
         <div>
-            <h2>Header</h2>
+            <header className={cx('wrapper')}>
+                <div className={cx('inner')}>
+                    {/* logo
+                    search */}
+                </div>
+            </header>
         </div>
     );
 };
